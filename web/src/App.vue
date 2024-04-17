653eba1b586b80abd96e934c8cff2c05c2568988
Mateusz Dybala <matidybala@gmail.com>
2024-04-17 09:57:17 +0300

    empty frontend

---
+++ web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <SearchBar />
+  <HomeView />
+
+</template>
+
+<script setup lang="ts">
+import HomeView from '@/views/HomeView.vue'
+import SearchBar from '@/components/SearchBar.vue'
+
+
+
+</script>
+
+<style>
+
+/* div {
+  border: 1px solid red;
+} */
+
+</style>
---
+++ web/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div class="main py-4">
+    <div class="flex justify-center">
+      <input type="text" placeholder="Search..."/>
+      <button>Search</button>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+// TODO Search with Spotify API
+
+</script>
+
+<style scoped>
+
+</style>
