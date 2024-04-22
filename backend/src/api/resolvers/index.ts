b5e8fbd8b4779797501f51ac7d0fe123750f0795
Mateusz Dybala <matidybala@gmail.com>
2024-04-22 20:43:27 +0300

    add wrapper for spotify requests

--- backend/src/api/resolvers/index.ts
+++ backend/src/api/resolvers/index.ts
@@ -1,4 +1,5 @@
 import userReslover from './userResolver';
 import reviewReslover from './reviewReslover';
+import spotifyResolver from './spotifyResolver';
 
-export default [userReslover, reviewReslover];
+export default [userReslover, reviewReslover, spotifyResolver];
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,53 @@
+import axios from 'axios';
 import app from './app';
 import mongoConnect from './utils/db';
+
+// TODO messy, should be moved/refactored
+const getSpotifyToken = async () => {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  var authOptions = {
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+    },
+    params: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  axios(authOptions)
+  .then(response => {
+    if (response.status === 200) {
+      const token = response.data.access_token;
+      process.env.SPOTIFY_ACCESS_TOKEN = token;
+      console.log('Retrieved access token successfuly');
+    } else {
+      console.error('Failed to retrieve access token:', response.status, response.statusText);
+    }
+  })
+  .catch(error => {
+    console.error('Error occurred while fetching access token:', error.message);
+  });
+}
+
 
 const port = process.env.PORT || 3000;
 (async () => {
   try {
     await mongoConnect();
+    await getSpotifyToken();
+    setInterval(async () => {
+      try {
+        await getSpotifyToken();
+        console.log('Spotify token refreshed.');
+      } catch (error: any) {
+        console.log('Error refreshing Spotify token:', error.message);
+      }
+    }, 3600 * 1000);
     app.listen(port, () => {
       /* eslint-disable no-console */
       console.log(`Listening: http://localhost:${port}`);
