5b521f4972cbbc3b6cd5afcae74940b7ee9b65cc
Mateusz Dybala <matidybala@gmail.com>
2024-04-30 17:17:58 +0300

    add auth server

---
+++ auth/src/api/models/userModel.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import {User} from '../../types/DBTypes';
+
+const userSchema = new mongoose.Schema<User>({
+  display_name: {
+    type: String,
+    reqired: true,
+  },
+  avatar_url: {
+    type: String,
+    reqired: true,
+  },
+  spotify_id: {
+    type: String,
+    reqired: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  access_token: {
+    type: String,
+    reqired: true,
+  },
+  refresh_token: {
+    type: String,
+    reqired: true,
+  },
+});
+
+export default mongoose.model<User>('User', userSchema);
