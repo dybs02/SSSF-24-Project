94db22eabd25caa55a053031abb12c35c4412992
Mateusz Dybala <matidybala@gmail.com>
2024-05-08 18:29:21 +0300

    add exposed headers

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, exposedHeaders: 'Set-Cookie' }));
 app.use(express.json());
 
 app.get<{}, MessageResponse>('/', (req, res) => {
